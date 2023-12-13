67fa080f2b21c5096e1fc7850a8fd73e8f670fb5
Ognjen Radenkovic <ognjenradenkovic14@gmail.com>
2023-12-13 20:15:00 +0100

    json->js

---
+++ reviews.js
@@ -0,0 +1,9 @@
+export const reviewsData = [
+    { "text": "Great coding skills! 100% recommend.", "rating": 5 },
+    { "text": "Not that bad, would give another chance though.", "rating": 3 },
+    { "text": "Not satisfied at all.", "rating": 1 },
+    { "text": "Not satisfied, needs more practice.", "rating": 2 },
+    { "text": "Overall good work, needs to work on time.", "rating": 3 },
+    { "text": "Lacks originality, but overall very efficient.", "rating": 4 },
+    { "text": "Very well designed games, 100% would recommend!.", "rating": 5 },
+];
